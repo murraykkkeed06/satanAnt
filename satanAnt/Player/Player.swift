6f1e1f29e3b7a4a2aef50e8bb283ff1d7f8e619a
murray <murrayliu35@gmail.com>
2021-03-24 18:24:44 +0800

    add bullet

---
+++ satanAnt/Player/Player.swift
@@ -0,0 +1,55 @@
+//
+//  Player.swift
+//  satanAnt
+//
+//  Created by 劉孟學 on 2021/3/24.
+//
+
+import Foundation
+import SpriteKit
+
+class Player: SKSpriteNode {
+    
+    var playerIsMoving = false
+    
+    var timer: Timer!
+    
+    let moveDistance: CGFloat = 0.02
+    
+    var timerSet = false
+    
+    init(){
+        let texture = SKTexture(imageNamed: "nakedAnt_2")
+        super.init(texture: texture, color: .clear, size: CGSize(width: 50, height: 50))
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func walkBy(handlePosition: CGPoint, handleMiddlePosition: CGPoint) {
+        //if !playerIsMoving {timer.invalidate(); return}
+        if timerSet {timer.invalidate()}
+        let vec = handlePosition - handleMiddlePosition
+        let vecNormal = vec.normalized()
+
+        timer = Timer.scheduledTimer(timeInterval: -1, target: self, selector: #selector(goingTo), userInfo: vecNormal, repeats: true)
+        timerSet = true
+    }
+    
+    @objc func goingTo(timer: Timer) {
+        let vecNormal = timer.userInfo as! CGPoint
+        
+        self.position.x += vecNormal.x * moveDistance
+        self.position.y += vecNormal.y * moveDistance
+        
+    }
+
+    
+    func stopWalk()  {
+        timer.invalidate()
+    }
+    
+    
+}
